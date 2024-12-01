87ac8087f5e251c0c3f20a59a900c6b50abc05b1
_evelyn <evelyn.krieg2@gmail.com>
2024-12-01 03:20:40 -0600

    oh wait yeah examples

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -53,6 +53,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, 31);
+        assert_eq!(result, Some(31));
     }
 }

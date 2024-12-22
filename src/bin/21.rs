b04dab09e2d6d8d83c138b11775ec736141b61ab
_evelyn <evelyn.krieg2@gmail.com>
2024-12-22 15:17:32 -0600

    i'm so fucking done with this day

--- src/bin/21.rs
+++ src/bin/21.rs
@@ -187,10 +187,4 @@ mod tests {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(126384));
     }
-
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
 }
